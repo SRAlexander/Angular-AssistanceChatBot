fb77ba7c585185cbaf357a52ec88f1e56e31e637
Scott Alexander <Scott.Alexander@Civica.co.uk>
2018-01-25 15:41:07 +0000

    Redirected the chat init location for the azure machine

--- src/app/modules/chatter/chat.service.ts
+++ src/app/modules/chatter/chat.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { IComment } from "./comment";
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable'; 
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
@@ -10,30 +10,29 @@ import { IWatsonResponse } from "app/modules/chatter/watson-response";
 @Injectable()
 export class ChatService {
 
-  //private _chatUrl : string = 'http://51.140.158.132/init';  
-  private _chatUrlSubmit = 'http://51.140.158.132/submit'
-private _chatUrl: string = '././api/conversation/conversation.json'; //<- Local data load for Get (/init)
+    private _chatUrl: string = 'http://51.140.158.132/init';
+    private _chatUrlSubmit = 'http://51.140.158.132/submit'
+    //private _chatUrl: string = '././api/conversation/conversation.json'; //<- Local data load for Get (/init)
 
-    constructor(private _http : HttpClient){
+    constructor(private _http: HttpClient) {
     }
-    
-    init(): Observable<IWatsonResponse>{
+
+    init(): Observable<IWatsonResponse> {
         return this._http.get<IWatsonResponse>(this._chatUrl + "")
-            .do(data => 
-                {
-                    console.log('Get: ' + JSON.stringify(data))
-                })
+            .do(data => {
+                console.log('Get: ' + JSON.stringify(data))
+            })
             .catch(this.handleError);
     }
 
-    postComment(comment) : Observable<IWatsonResponse>{
+    postComment(comment): Observable<IWatsonResponse> {
 
         let body = JSON.stringify(comment);
-        let headers = new HttpHeaders({'Content-Type': 'application/json'});
-        let options = {headers:headers};
+        let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        let options = { headers: headers };
 
         return this._http.post<IWatsonResponse>(this._chatUrlSubmit, body, options)
-            .do(data => 
+            .do(data =>
                 console.log("Post" + JSON.stringify(data))
             )
             .catch(this.handleError);
